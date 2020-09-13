a2ddd8c9986e20a88f5b2ffdc844ca55a6142e6a
griends1 <Steelpan1>
2020-09-13 20:10:01 +0200

    Added Repo

---
+++ app/src/main/java/com/example/madlevel3example/Repository/ReminderRepository.kt
@@ -0,0 +1,32 @@
+package com.example.madlevel3example.Repository
+
+import android.content.Context
+import com.example.madlevel2example.Reminder
+import com.example.madlevel3example.DAO.ReminderDao
+import com.example.madlevel3example.ReminderRoomDatabase
+
+class ReminderRepository(context: Context) {
+
+    private var reminderDAO: ReminderDao
+
+    init {
+        val reminderRoomDatabase = ReminderRoomDatabase.getDatabase(context)
+        reminderDAO = reminderRoomDatabase!!.reminderDao()
+    }
+
+    fun getAllReminders(): List<Reminder> {
+        return reminderDAO.getAllReminders()
+    }
+
+    fun insertReminder(reminder: Reminder) {
+        reminderDAO.insertReminder(reminder)
+    }
+
+    fun deleteReminder(reminder: Reminder) {
+        reminderDAO.deleteReminder(reminder)
+    }
+
+    fun updateReminder(reminder: Reminder) {
+        reminderDAO.updateReminder(reminder)
+    }
+}
